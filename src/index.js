5ee1d309e83fe7069050756a179e9d5dc8a41268
Riina Backman <gr188369@gradia.fi>
2021-03-23 16:54:06 +0200

    Updated
    New components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+//Simple react tests
+
+//Simple text from the variable element to the 'root'
+//const element = <h1>Hello world</h1>;
+//ReactDOM.render(element, 'root');
+
+//Simple list element.
+const listElement = (
+    <ul>
+        <li>Shiba</li>
+        <li>Hokkaido</li>
+        <li>Kishu</li>
+        <li>Shikoku</li>
+        <li>Kai</li>
+        <li>Akita</li>
+    </ul>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(listElement, document.getElementById('root'));
+
+//Simple function element that's called in the root when page is loaded.
+//NOT CALLED
+function Akita(){
+    return <h2>Hello, I'm an akita inu!</h2>
+}
+
+//Class function element that has constructor.
+//NOT CALLED
+class AkitaInu extends React.Component {
+    constructor(){
+        super();
+        this.state = {color: "red"}//this works a bit like variable
+    }
+    render() {
+        return <h2>Hello, I'm {this.state.color} akita inu!</h2>;
+    }
+}
+
+//component in a component!
+//Akita2 componenet is called in the Heart component
+class Akita2 extends React.Component {
+    render() {
+        return <h2>Akita!</h2>;
+    }
+}
+
+class Heart extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Who lives in your heart?</h1>
+                <Akita2 />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Heart />, document.getElementById('root'));
---
+++ src/testcomponent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Akita(){
+    return <h2>Hello I'm an akita inu!</h2>
+}
+
+ReactDOM.render(<Akita />, document.getElementById('root'));
